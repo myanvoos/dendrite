772c1409f34adc02f3d3aac3bb8bfc463d318763
mylanvoos <mavdbt@gmail.com>
2024-11-17 22:14:41 +1300

    feat: render a basic Ratatui example Added Ratatui to test terminal-based UI components.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
-use crossterm::event::{self, Event};
-use ratatui::{text::Text, Frame};
+use std::io;
 
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Paragraph, Block, Widget},
+    DefaultTerminal, Frame,
+};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Ratatui! (press 'Esc' to quit)")
+                .white()
+                .on_black();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Esc {
+                return Ok(());
+            }
+        }
+    }
 }
